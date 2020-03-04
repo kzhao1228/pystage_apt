5ab93f570d7fa9410d9e41b7bcde1c6b0c1ae4d4
Kaixiang Zhao <35736935+kzhao1228@users.noreply.github.com>
2020-03-04 19:37:02 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -19,15 +19,23 @@ PACKAGES = [
     'stage/ctrl_msg'
 ]
 
-setup(name = 'PyStage-APT',
-      version = '0.0.0',
+LONG_DESCRIPTION = 'pystage-apt is a library to communicate with various Thorlabs\' APT single-channel controllers ' +\
+    'and control different types of Thorlabs\' actuator motors. ' +\
+    'It contains a large collection of motor control messages obtained from Thorlabs APT Controllers ' +\
+    'Host-Controller Communications Protocol. This document describes the low-level communications ' +\
+    'protocol and commands used between the host PC and controller units within the APT family. ' +\
+    'Those messages are included in a series of python files and are stored in a folder named ctrl_msg. '
+    
+
+setup(name = 'pystage_apt',
+      version = '0.0',
       url = 'https://github.com/kzhao1228/pystage_apt',
       license = 'MIT',
       author = 'Kaixiang Zhao',
       author_email = 'kz1619@ic.ac.uk',
-      description = 'A Python library for Thorlabs\' APT stage controllers',
-      long_description = 'pystage_apt is a python library to control various Thorlabs\' APT stage controllers with single channel',
-      platforms = '[Linux]',
+      description = 'A python library for Thorlabs\' APT single-channel controllers',
+      long_description = LONG_DESCRIPTION,
+      platforms = ['Linux'],
       packages = PACKAGES,
       install_requires = ['pyusb>=1.0.0a','pyserial>=2.7'],
       classifiers = CLASSIFIERS,
